8482859ba8436767c32d9b89256f343b6c15c7c2
eshohag <eshohag@outlook.com>
2022-12-23 22:47:57 +0600

    periodStart

--- TrackingDataChanges/Controllers/TemporalController.cs
+++ TrackingDataChanges/Controllers/TemporalController.cs
@@ -14,9 +14,14 @@ namespace TrackingDataChanges.Controllers
         }
         public IActionResult Index()
         {
-            //var startTime = Convert.ToDateTime("2022-12-23 13:30:50.6736582");
-            //var students = _context.Students.TemporalAsOf(startTime);
-            var students = _context.Students.TemporalAll().Select(a=> new StudentHistory() { Id=a.Id, Name=a.Name, PeriodStart= EF.Property<DateTime>(a, "PeriodStart"), PeriodEnd= EF.Property<DateTime>(a, "PeriodEnd") }).ToList();
+            //var periodStart = Convert.ToDateTime("2022-12-23 13:30:50.6736582");
+            //var students = _context.Students.TemporalAsOf(periodStart);
+            var students = _context.Students.TemporalAll()
+                .Select(a=> new StudentHistory() { 
+                    Id=a.Id, 
+                    Name=a.Name, 
+                    PeriodStart= EF.Property<DateTime>(a, "PeriodStart"),
+                    PeriodEnd= EF.Property<DateTime>(a, "PeriodEnd") }).ToList();
 
             return View(students);
         }
